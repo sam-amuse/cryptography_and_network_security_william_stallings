a2582a66d63120c195a3f6083cbc00c02c32ac72
Samir BEN DODO <samir.bendodo@gmail.com>
2019-10-25 00:36:03 +0200

    simplification of my euclidean algorithm code and addition an optimal version of the euclidean algorithm in python

--- python/chapter-2-number-theory/euclidean_algorithm.py
+++ python/chapter-2-number-theory/euclidean_algorithm.py
@@ -5,39 +5,19 @@ Euclidean algorithm  is procedure to determine the greatest common divisor or tw
 '''
 from math import floor
 
-import logging
-import sys
-log = logging.getLogger(__name__)
-out_hdlr = logging.StreamHandler(sys.stdout)
-out_hdlr.setLevel(logging.DEBUG)
-log.addHandler(out_hdlr)
-log.setLevel(logging.DEBUG)
-#log.setLevel(logging.INFO)
-
-
 print("Enter A and B to find GCD(A,B)")
 
 a = int( input("A = "))
 b = int( input("B = "))
 
-if a<b :
-    a,b = b,a #swap a and b
-
 a_temp =a
 b_temp =b
-
 remainder =1
 # R : remainder, Q : quotient
 # equation : R(n-2) = Q(n)*R(n-1) + R(n)   
 while remainder != 0 :
-    quotient = floor(a_temp/b_temp)
     remainder = a_temp%b_temp
     a_temp=b_temp
     b_temp= remainder
-    
-    log.debug("new A = %d", a_temp)
-    log.debug("new B = %d", b_temp)
-    log.debug("A/B = %d", quotient)
-    log.debug("Amod(B) %d= ", remainder)
-    
+   
 print ("GCD(",a,",",b,") =", a_temp)
---
+++ python/chapter-2-number-theory/euclidean_algorithm_optimal.py
@@ -0,0 +1,18 @@
+'''
+Created on 23 oct. 2019
+Euclidean algorithm  is procedure to determine the greatest common divisor or two positive integers
+@author: Samir BEN DODO
+'''
+
+from math import floor
+
+def euclid_algo(a, b, verbose):
+    while b:
+        a, b = b, a % b    
+        if verbose and b != 0: print('%s = %s * %s + %s' % (a, floor(a/b), b, a % b))
+    if verbose: print('\nGCD is %s \n' % a)
+    return a
+
+euclid_algo(283, 32874, False)
+euclid_algo(8334,93274, True)
+euclid_algo(3767,7919, True) 
